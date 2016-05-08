9a5345a65667399afb6546d599da4dfa71a2e7be
Myles <Myles>
2016-05-08 14:47:17 +0800

    add dynmic class base on bootstrap critical point

--- handson_v0.2/js/master.js
+++ handson_v0.2/js/master.js
@@ -19,36 +19,3 @@ $(document).ready(function(){
 		}
 	});
 });
-
-function boolean validateEmail(String s){
-	var boolean result;
-    var pattern = /w/i;
-	result=s.match(pattern);
-	return result;
-}
-
-function boolean validatePhone(String s){
-	alert("ping function with name");
-}
-
-function boolean validateYYYYYMM(String s){
-	alert("ping function with name");
-}
-
-function String postData(String jsonOfForm){
-	var String result;
-	var boolean isJson;
-	isJosn=jsonValidation(jsonOfForm);
-	if (isJson){
-		$.post("/dynamic/ping-servlet",jsonOfForm,function(data){result=data});
-	}else{
-		result="wrong json format";
-	}
-	return result;
-}
-
-function boolean jsonValidation(String s){
-	var boolean result;
-	//operation
-	return result;
-}
