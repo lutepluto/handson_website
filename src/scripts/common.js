18244f8bef4e0a40412234dc77c59c6284a4cb42
lutepluto <lutepluto@gmail.com>
2019-03-11 04:26:26 +0800

    add language detector

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -1,18 +1,19 @@
 import $ from 'jquery'
 import i18next from 'i18next'
+import LngDetector from 'i18next-browser-languagedetector';
 import resources from 'locales'
 
 import 'styles/styles.css'
 import 'styles/carousel.css'
 
-i18next.init({
-  lng: 'en',
-  fallbackLng: 'zh',
-  ns: ['common', 'home', 'application', "join", 'about'],
-  defaultNS: 'home',
-  debug: 'true',
-  resources
-})
+i18next
+  .use(LngDetector)
+  .init({
+    ns: ['common', 'home', 'application', "join", 'about'],
+    defaultNS: 'home',
+    debug: 'true',
+    resources
+  })
 
 $(window).resize(function(){
   if ($('#logo > span').css('display') === 'none') {
