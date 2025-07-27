32692a8ee2f29cd3c479c639fcf9d5ab36108fe2
lute <lutepluto@gmail.com>
2025-07-27 16:30:01 +0800

    chore: Scaffold nextjs

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    plugins: [
-        require('autoprefixer')({
-            flexbox: true,
-            browsers: ['>= 0.5%', 'ie >= 10']
-        })
-    ]
-}
---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { ReactNode } from 'react';
+
+export default function RootLayout({
+  children
+}: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Home page of Handson, a social entreprise devoted on connecting technology and social issue" />
+        <meta name="author" content="Myles Ieong" />
+        <meta name="robots" content="index, follow" />
+      </head>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
